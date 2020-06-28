69ee8f2fdacdce4869434347a49754c206449e6e
wtycjan <56653553+wtycjan@users.noreply.github.com>
2020-06-28 13:43:03 +0200

    initial commit

---
+++ Assets/Scripts/Sounds.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sounds : MonoBehaviour
+{
+    public AudioClip audio1;
+    AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void Sound1()
+    {
+        print("sound");
+        audioSource.PlayOneShot(audio1);
+    }
+}
---
+++ Assets/Scripts/UI_Buttons.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UI_Buttons : MonoBehaviour
+{
+    public Image popup;
+    public Text text;
+
+    private bool isClicked=false;
+    public void PopUp_Btn()
+    {
+        popup.gameObject.SetActive(true);
+    }
+    public void ClosePopup()
+    {
+        popup.gameObject.SetActive(false);
+    }
+    public void Text_Btn()
+    {
+        if(!isClicked)
+            StartCoroutine("ShowText");
+    }
+
+    private IEnumerator ShowText()
+    {
+        isClicked = true;
+        text.gameObject.SetActive(true);
+        yield return new WaitForSeconds(3f);
+        text.gameObject.SetActive(false);
+        isClicked = false;
+    }
+
+}
